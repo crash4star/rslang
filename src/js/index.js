e43f7322bc986afa9163d2ebe3aba42514d5bc4c
TatarskiyPavel <Pasha.Tatarsky@gmal.com>
2020-06-19 17:55:23 +0300

    init: english-puzzle minigame task

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,10 @@
 import '../css/style.css';
 import '../css/style.scss';
+import Model from './Model/Model';
+import View from './View/View';
+import Controller from './Controller/Controller';
+import UserInterface from './View/UserInterface';
+
+const app = new Controller(new Model(), new View(new UserInterface()));
+
+console.log(app);
