d95b836e065b8642133cfdc9095279ae24b84099
crash4star <crashmoney@yandex.ru>
2020-06-26 13:36:09 +0300

    feat: fix view file

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 import '../css/style.css';
 import '../css/style.scss';
 
-import SavannahView from './savannah/view';
+import SavannahView from './savannah/savannah-view';
 
 const savannah = new SavannahView('#root');
 savannah.start();
