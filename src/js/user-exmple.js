c7388c332978668acc1c2386b10fe1be7aa922bb
Nikita <47321264+crash4star@users.noreply.github.com>
2020-06-29 21:58:36 +0300

    feat: add user example object for db

---
+++ src/js/user-exmple.js
@@ -0,0 +1,41 @@
+const user = {
+    games: {
+        savannah: [
+            {
+                date: '',
+                total: '',
+                success: ''
+            }
+        ],
+        sprint: [
+            {
+                date: '',
+                total: '',
+                success: '',
+                settings: {
+                    auto: true,
+                    showImg: true,
+                    showTranslate: true,
+                    playAudio: true
+                }
+            }
+        ]
+    },
+    amount: 'success x all',
+    createdWords: [
+        {
+            id: '',
+            title: ''
+        }
+    ],
+    settings: {
+        amountNew: '',
+        totalCards: '',
+        difficult: ''
+    },
+    theme: {
+        current: '',
+        custom: {}
+    }
+}
+
