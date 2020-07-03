bb78a37087c2b31b0b002de9d63b741f5665f77a
crash4star <crashmoney@yandex.ru>
2020-07-03 19:14:22 +0300

    feat: update models

--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,20 @@
 import '../css/style.css';
 import '../css/style.scss';
 
-import Model from './savannah/model';
 import View from './savannah/view';
-import Api from './savannah/api';
+import Api from './models/Api';
+
+import ProfileOptions from './models/ProfileOptions';
+import Settings from './models/Settings';
 
 const BASE_URL = `https://afternoon-falls-25894.herokuapp.com`;
 
 // const mode = new Model(new Api(BASE_URL));
+// mode.getUserWords().then(data => console.log(data))
 
-// mode.getWords(2, 1).then(data => console.log(data));
 
-
-const viewTest = new View();
-viewTest.render(['test', 'qwerty', 'asd', 'zzz']);
- 
+const stat = new Settings(new Api(BASE_URL));
+stat.getUserSettings().then(data => console.log(data))
 
 
 
@@ -24,11 +24,9 @@ viewTest.render(['test', 'qwerty', 'asd', 'zzz']);
 
 
 
-
-
-
-
-
+// const viewTest = new View();
+// viewTest.render(['test', 'qwerty', 'asd', 'zzz']);
+// { "email": "tester-crash@mail.com", "password": "3kDVsPXp333_"}
 
 
 
--- src/js/utils/sendRequest.js
+++
@@ -1,7 +0,0 @@
-function sendRequest(method, url) {
-  return fetch(url).then(response => {
-    return response.json();
-  });
-}
-
-export default sendRequest;
