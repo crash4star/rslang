cc0aa0b843dc0c015f7b1fffd9ea09a540d0f7f3
crash4star <crashmoney@yandex.ru>
2020-07-08 15:24:05 +0300

    feat: fix start game method

--- src/js/index.js
+++ src/js/index.js
@@ -9,5 +9,5 @@ import View from './savannah/ViewSavannah';
 const BASE_URL = `https://afternoon-falls-25894.herokuapp.com`;
 
 const app = new ControllerApp(new Words(new Api(BASE_URL)), new View());
-app.start();
+// app.start();
 
