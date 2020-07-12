48570fe6703f20e5867942f545ec2883332b1685
KateProtasevich <KrishniovaKate@mail.ru>
2020-07-13 00:41:01 +0300

    feat: add defaults settings, statistics and const

---
+++ src/js/main-game/assets/defaultLearningSettings.js
@@ -0,0 +1,32 @@
+const defaultLearningSettings = {
+  "wordsPerDay": 20,
+  "optional": {
+    "settingsProfile": {
+      "theme": 0,
+      "difficult": 0
+    },
+    "linguist": {
+      "learn": {
+        "maxCardsPerDay": 20,
+        "wordsComposition": "new",
+        "hardInterval": 10,
+        "goodInterval": 1,
+        "easyInterval": 4,
+        "learnedInterval": 7
+      },
+      "cards": {
+        "showTranslateWord": true,
+        "showExampleSentence": true,
+        "showExplanationSentence": true,
+        "showImage": true,
+        "showTranscription": true,
+        "addAnswerBtns": true,
+        "addIntervalBtns": true,
+        "addDeleteBtn": true,
+        "addSpecialBtn": true
+      }
+    }
+  }
+};
+
+export default defaultLearningSettings;
