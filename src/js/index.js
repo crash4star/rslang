5139a21e9642ee60d325d67b50f52b5b31ab70b7
TatarskiyPavel <Pasha.Tatarsky@gmal.com>
2020-07-14 14:59:20 +0300

    fix: fix merge problem second time

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,8 @@
+  
 import '../css/style.scss';
-<<<<<<< HEAD
 import renderStartPage from './start page/startPage';
 import renderMainPage from './main page/mainPage';
-import { refreshToken} from './utils/authorization'
+import { refreshToken} from './utils/authorization';
 
 const updateTokenPeriod = 3 * 3600 * 1000;
 
@@ -21,26 +21,3 @@ setInterval(async () => {
 }, updateTokenPeriod);
 
 renderPage();
-=======
-import renderStartPage from './start page/startPage';
-import renderMainPage from './main page/mainPage';
-import { refreshToken} from './utils/authorization'
-
-const updateTokenPeriod = 3 * 3600 * 1000;
-
-export default async function renderPage() {
-    if (!localStorage.getItem('userId')) {
-        renderStartPage();
-    } else {
-        renderMainPage();
-    }
-}
-
-setInterval(async () => {
-    if (!await refreshToken()) {
-        renderStartPage();
-    }
-}, updateTokenPeriod);
-
-renderPage();
->>>>>>> ef8a14b7f401a0014f1d386fc43eea5afb2662d8
