ae685857eda0b5b2eeb8a4f05b67ff579b973a0a
TatarskiyPavel <Pasha.Tatarsky@gmal.com>
2020-07-14 17:00:40 +0300

    feat: create model abstract class

---
+++ src/js/puzzle/Model/Model.js
@@ -0,0 +1,12 @@
+// import Utils from './Utils';
+
+class Model {
+  constructor(options, words, paintings, statistic) {
+    this.options = options;
+    this.words = words;
+    this.paintings = paintings;
+    this.statistic = statistic;
+  }
+}
+
+export default Model;
