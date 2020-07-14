4ede3875516822e5af89c4a30c851695f710ffed
TatarskiyPavel <Pasha.Tatarsky@gmal.com>
2020-07-14 16:41:08 +0300

    refactor: add styles

--- src/js/View/Container.js
+++
@@ -1,9 +0,0 @@
-import UIComposite from './UIComposite';
-
-class Container extends UIComposite {
-  constructor(id, styles) {
-    super('div', id, '', '', styles);
-  }
-}
-
-export default Container;
